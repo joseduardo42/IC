cb169f9d3ff70feaa3ae8cdc17cd46d50f5564a3
joseduardo42 <joseduardosilva.42@gmail.com>
2021-07-16 12:58:17 -0300

    por malhas (error)

--- Ex3.py
+++ Ex3.py
@@ -9,17 +9,17 @@ RL = 50
 Vc10 = 10
 Vc20 = 20
 Vs = 0
-w=100
-A = 100
+w = 100000
+A = 1.2
 deltat = 1/(20 * w)
 tf = 5*1/w
-C1 = 10*10**(-12)
+C1 = 10**(-11)
 C2 = 10**(-6)
 
 #construindo matriz a partir da análise de malhas
 def func(x):
     return[ Ra*x[0] + Vc10,
-            -Vc10 + Vc20 + x[1]*RL ]
+            -Vc10 + Vc20 + x[1]*RL]
 
 y = fsolve(func, [0, 0])
 
@@ -48,7 +48,7 @@ for t in t_sim:
     Vs = (100*np.sin(w*t)) #tensão da fonte no tempo desejado
 
     def func(x):
-        return[((0.1*np.sign(Vs))/(1+(1.8/np.abs(Vs))**5)**(1/5)) + Ra*(x[0]-x[1]),
+        return[((0.1*np.sign(Vs)) / (1 + (1.8/abs(Vs))**5)**(1/5)) + Ra*(x[0]-x[1]),
                 Ra*(x[1]-x[0]) + Vc10 + (1/(2*C1))*deltat*((x[1] - x[2]) + ic10),
                 -(1/(2*C1))*deltat*((x[1] - x[2]) + ic10) + (1/(2*C2))*deltat*(x[2] + ic20)]
     
@@ -64,9 +64,8 @@ for t in t_sim:
     ic10 = y2 - y3
     ic20 = y3
 
-    Vc10 = (1/(2*C1))*deltat*((y[1] - y[2]) + ic10)
-    Vc20 = (1/(2*C2))*deltat*(y[2] + ic20)
-
+    result_vc1.append (Vc10)
+    result_vc2.append (Vc20)
     result_ic1.append (ic10)
     result_ic2.append (ic20)
 
