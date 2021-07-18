c4e596e593ff46a125eb7baa895d155395174175
joseduardo42 <joseduardosilva.42@gmail.com>
2021-07-17 23:02:26 -0300

    V2 - malhas

--- Ex3.py
+++ Ex3.py
@@ -42,33 +42,30 @@ result_vc2.append (Vc20)
 result_ic1.append (ic10)
 result_ic2.append (ic20)
 #variavei que irão ser usadas no for, assumirão outros valores a cada iteração
-y1 = 0
-y2 = ic10
-y3 = ic20
+y1 = ic10
+y2 = ic20
 
 t_sim = np.arange(deltat, tf+deltat, deltat)#tempo de simulação
 t_plot = np.arange(0, tf+deltat, deltat) #vetor com todas posições a ser plotado
 
 for t in t_sim:
 
-    Vs = -(Vm1*np.sin(2*pi*f1*t) + Vm2*np.sin(2*pi*f2*t)) #tensão da fonte no tempo desejado
+    Vs = (Vm1*np.sin(2*pi*f1*t) + Vm2*np.sin(2*pi*f2*t)) #tensão da fonte no tempo desejado
     #equações da segunda analise
     def func(x):
-        return[((0.1*np.sign(Vs)) / (1 + (1.8/abs(Vs))**5)**(1/5)) + Ra*(x[0]-x[1]),
-             Ra*(x[1]-x[0]) + Vc10 + (1/(2*C1))*deltat*((x[1] - x[2]) + ic10),
-               -(1/(2*C1))*deltat*((x[1] - x[2]) + ic10) + (1/(2*C2))*deltat*(x[2] + ic20)]
+        return[Ra*(x[0]-((0.1*np.sign(Vs)) / ((1 + (1.8/abs(Vs))**5)**(1/5)))) + Vc10 + (1/(2*C1))*deltat*((x[0] - x[1]) + ic10),
+               -(Vc10 + 1/(2*C1)*deltat*((x[0] - x[1]) + ic10)) + (Vc20 + 1/(2*C2)*deltat*(x[1] + ic20)) + RL*x[1]]
     
-    y = fsolve(func, [y1, y2, y3])
+    y = fsolve(func, [y1, y2])
     #valores das tensões para a próxima iteração
-    Vc10 = (1/(2*C1))*deltat*((y[1] - y[2]) + ic10)
-    Vc20 = (1/(2*C2))*deltat*(y[2] + ic20)
+    Vc10 = (Vc10 + 1/(2*C1)*deltat*((y[0] - y[1]) + ic10))
+    Vc20 = (Vc20 + 1/(2*C2)*deltat*(y[1] + ic20))
     #atualizando variáveis do chute inicial
     y1 = float (y[0])
     y2 = float (y[1])
-    y3 = float (y[2])
     #valores das correntes para a próxima iteração
-    ic10 = y2 - y3
-    ic20 = y3
+    ic10 = y1 - y2
+    ic20 = y2
     #armazenando variáveis
     result_vc1.append (Vc10)
     result_vc2.append (Vc20)
