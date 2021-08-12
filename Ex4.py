43d41f78b513b5a21fd8f9827b9220b4f5a64beb
joseduardo42 <86281584+joseduardo42@users.noreply.github.com>
2021-08-12 17:37:37 -0300

    Update Ex4.py

--- Ex4.py
+++ Ex4.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy.linalg import inv
 from scipy.optimize import fsolve
 
+#initial params
 w = 100
 h = 2
 Ra = 10**3
@@ -20,6 +21,7 @@ F = np.array([[1, sin(2*pi*0*1/(2*h+1)),cos((2*pi*0*1/(2*h+1))), sin(2*pi*0*2/(2
 
 F_inv = inv(F)
 
+#omega matrix
 omega = np.zeros((5,5))
 omega[1, 2] = -w; omega[2, 1] = w; omega[3, 4] = -2*w; omega[4, 3] = 2*w
 
