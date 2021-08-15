b256908cea922e0e54cb45bf5f908090018e71a0
joseduardo42 <86281584+joseduardo42@users.noreply.github.com>
2021-08-15 15:05:55 -0300

    with transient analysis

--- Ex4.py
+++ Ex4.py
@@ -14,14 +14,13 @@ Ra = 10**3
 C1 = 10*10**(-12)
 C2 = 10**(-6)
 RL = 50
-Ra = 10**3
 A = 100
 
-F = np.array([[1, sin(2*pi*0*1/(2*h+1)), cos((2*pi*0*1/(2*h+1))), sin(2*pi*0*2/(2*h+1)), cos((2*pi*0*2/(2*h+1)))],
-              [1, sin(2*pi*1*1/(2*h+1)), cos((2*pi*1*1/(2*h+1))), sin(2*pi*1*2/(2*h+1)), cos((2*pi*1*2/(2*h+1)))],
-              [1, sin(2*pi*2*1/(2*h+1)), cos((2*pi*2*1/(2*h+1))), sin(2*pi*2*2/(2*h+1)), cos((2*pi*2*2/(2*h+1)))],
-              [1, sin(2*pi*3*1/(2*h+1)), cos((2*pi*3*1/(2*h+1))), sin(2*pi*3*2/(2*h+1)), cos((2*pi*3*2/(2*h+1)))],
-              [1, sin(2*pi*4*1/(2*h+1)), cos((2*pi*4*1/(2*h+1))), sin(2*pi*4*2/(2*h+1)), cos((2*pi*4*2/(2*h+1)))]])
+F = np.array([[1, sin(2*pi*0*1/(2*h+1)), cos((2*pi*0*1/(2*h+1))), sin(2*pi*0*2/(2*h+1)), cos((2*pi*0*2)/(2*h+1))],
+              [1, sin(2*pi*1*1/(2*h+1)), cos((2*pi*1*1/(2*h+1))), sin(2*pi*1*2/(2*h+1)), cos((2*pi*1*2)/(2*h+1))],
+              [1, sin(2*pi*2*1/(2*h+1)), cos((2*pi*2*1/(2*h+1))), sin(2*pi*2*2/(2*h+1)), cos((2*pi*2*2)/(2*h+1))],
+              [1, sin(2*pi*3*1/(2*h+1)), cos((2*pi*3*1/(2*h+1))), sin(2*pi*3*2/(2*h+1)), cos((2*pi*3*2)/(2*h+1))],
+              [1, sin(2*pi*4*1/(2*h+1)), cos((2*pi*4*1/(2*h+1))), sin(2*pi*4*2/(2*h+1)), cos((2*pi*4*2)/(2*h+1))]])
 
 F_inv = inv(F)
 
@@ -44,12 +43,13 @@ def circuit_equations(V):
     #definition of amplitude source and Va in time-domain
     A_amplitude = np.array([0, 0, A, 0, 0])
     non_linear = F@Va
+    vc2 = Vb - Vc
 
     return np.concatenate([
         Va - A_amplitude,
-        F_inv@((0.1*np.sign(non_linear))/((1 + (1.8/abs(non_linear))**5)**(1/5))) - (1/Ra)*Vb + C1*(omega@(Vb-Vc)),
-        C2*omega@(Vb - Vc) - (1/RL)*Vc 
-    ])
+        (F_inv@((0.1*np.sign(non_linear))/((1 + (1.8/abs(non_linear))**5)**(1/5)))) - (1/Ra)*Vb - (C1*omega)@Vb - (C2*omega)@(vc2),
+        C2*omega@(vc2) - (1/RL)*Vc 
+    ]) 
 
 #starting estimate and solve the system of nonlinear equations
 amplitudes_guess = np.zeros(15)
@@ -62,36 +62,36 @@ t_sim = np.arange(0, tf+deltat, deltat)#time simulation
 #vectors to storage results
 results_va = []
 results_vb = []
-results_vc2 = []
+results_vc = []
 
 #waveforms of HB
 for t in t_sim:
-    Va_time =  y[0] + y[1]*sin(w*t) + y[2]*np.cos(w*t) + y[3]*sin(2*w*t) + y[4]*cos(2*w*t) 
-    Vb_time = y[5] + y[6]*sin(w*t) + y[7]*cos(w*t) + y[8]*sin(2*w*t) + y[9]*cos(2*w*t) 
-    Vc_time = y[10] + y[11]*sin(w*t) + y[12]*cos(w*t) + y[13]*sin(2*w*t) + y[14]*cos(2*w*t) 
-
-    Vc2_time = (y[5] - y[10]) + (y[6]- y[11])*sin(w*t) + (y[7] - y[12])*cos(w*t) + (y[8] - y[13])*sin(2*w*t) + (y[9] - y[14])*cos(2*w*t) 
+    Va_time =  y[0] +  y[1]*sin(w*t)  + y[2]*cos(w*t)  + y[3]*sin(2*w*t)  + y[4]*cos(2*w*t) 
+    Vb_time =  y[5] +  y[6]*sin(w*t)  + y[7]*cos(w*t)  + y[8]*sin(2*w*t)  + y[9]*cos(2*w*t)
+    Vc_time =  y[10] + y[11]*sin(w*t) + y[12]*cos(w*t) + y[13]*sin(2*w*t) + y[14]*cos(2*w*t) 
+    dependent_source = ((0.1*np.sign(Va_time))/((1 + (1.8/abs(Va_time))**5)**(1/5)))
     
     results_va.append (Va_time)
     results_vb.append (Vb_time)
-    results_vc2.append (Vc2_time)
+    results_vc.append (Vc_time)
 
-plt.plot (t_sim, results_va)
-plt.title ('Tensão nó A')
-plt.ylabel ('(V)')
-plt.xlabel ('Tempo (milisegundos)')
-plt.grid()
-plt.show ()
-plt.plot (t_sim, results_vb)
-plt.title ('Tensão no Capacitor 1')
-plt.ylabel ('(V)')
-plt.xlabel ('Tempo (milisegundos)')
-plt.grid()
-plt.show ()
+#plt.plot (t_sim, results_va)
+#plt.title ('Corrente da fonte controlada')
+#plt.ylabel ('(V)')
+#plt.xlabel ('Tempo (milisegundos)')
+#plt.grid()
+#plt.show ()
 
-plt.plot (t_sim, results_vc2)
-plt.title ('Tensão no Capacitor 2')
-plt.ylabel ('(V)')
-plt.xlabel ('Tempo (milisegundos)')
-plt.grid()
-plt.show ()
+#plt.plot (t_sim, results_vb)
+#plt.title ('Tensão no Capacitor 1')
+#plt.ylabel ('(V)')
+#plt.xlabel ('Tempo (milisegundos)')
+#plt.grid()
+#plt.show()
+
+#plt.plot (t_sim, results_vc2)
+#plt.title ('Tensão no Capacitor 2')
+#plt.ylabel ('(V)')
+#plt.xlabel ('Tempo (milisegundos)')
+#plt.grid()
+#plt.show ()
