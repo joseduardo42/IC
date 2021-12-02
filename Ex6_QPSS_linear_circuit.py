f2a81a16375d60f00cff1ee0b2fa20217cc5bed9
Jose Eduardo <joseduardosilva.42@gmail.com>
2021-12-02 19:07:10 -0300

    02-12

--- Ex6_QPSS_linear_circuit.py
+++ Ex6_QPSS_linear_circuit.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sin, cos, pi
-from numpy import linalg, pi
+from numpy import arange, concatenate, dot, linalg, pi
 from scipy.optimize.minpack import fsolve
 
 #ICs
@@ -18,105 +18,106 @@ w2 = 2*pi*f2
 deltat = 1/(100 * f1)
 T1 = (1/f1)
 T = (1/f2)
-h = 2
 
-#frequency -> time
-gamma_inv = np.array([[1, sin(2*pi*0*1/(2*h+1)), cos(2*pi*0*1/(2*h+1)), sin(2*pi*0*2/(2*h+1)), cos((2*pi*0*2)/(2*h+1))],
-                      [1, sin(2*pi*1*1/(2*h+1)), cos(2*pi*1*1/(2*h+1)), sin(2*pi*1*2/(2*h+1)), cos((2*pi*1*2)/(2*h+1))],
-                      [1, sin(2*pi*2*1/(2*h+1)), cos(2*pi*2*1/(2*h+1)), sin(2*pi*2*2/(2*h+1)), cos((2*pi*2*2)/(2*h+1))],
-                      [1, sin(2*pi*3*1/(2*h+1)), cos(2*pi*3*1/(2*h+1)), sin(2*pi*3*2/(2*h+1)), cos((2*pi*3*2)/(2*h+1))],
-                      [1, sin(2*pi*4*1/(2*h+1)), cos(2*pi*4*1/(2*h+1)), sin(2*pi*4*2/(2*h+1)), cos((2*pi*4*2)/(2*h+1))]])
+for h in range(2,10):
 
-#frequency -> time, one period ahead
-gamma_inv_T1 = np.array([[1, sin(1*(2*pi*f2*(0*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(0*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(0*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(0*T/(2*h+1)+T1)))],
-                         [1, sin(1*(2*pi*f2*(1*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(1*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(1*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(1*T/(2*h+1)+T1)))],
-                         [1, sin(1*(2*pi*f2*(2*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(2*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(2*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(2*T/(2*h+1)+T1)))],
-                         [1, sin(1*(2*pi*f2*(3*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(3*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(3*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(3*T/(2*h+1)+T1)))],
-                         [1, sin(1*(2*pi*f2*(4*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(4*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(4*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(4*T/(2*h+1)+T1)))]])
+  #frequency -> time
+  gamma_inv = np.array([[1] + [f(2*pi*(i)*(j+1)/(2*h+1)) for j in range(h) for f in (sin, cos)] for i in range(2*h+1)])
 
-#time -> frequency
-gamma = linalg.inv(gamma_inv)
+  #frequency -> time, one period ahead
+  gamma_inv_T1 = np.array([[1] + [f(2*pi*f2*(j+1)*((i)*T/(2*h+1) + T1)) for j in range(h) for f in (sin, cos)] for i in range(2*h+1)])
 
-#delay matrix
-D = gamma_inv_T1@gamma
+  #time -> frequency
+  gamma = linalg.inv(gamma_inv)
 
-#vectors to storage results
-results_vc = []
+  #delay matrix
+  D = gamma_inv_T1@gamma
 
-transient_result = np.zeros(5)
+  #vectors to storage results
+  results_vc = []
 
-def QPSS(V_shooting):
+  transient_result = np.zeros(2*h+1)
 
-  shooting_voltage = np.zeros(5)
-  for j in range(5):
-      shooting_voltage[j] = V_shooting[j]
-  print (shooting_voltage)
+  def QPSS(V_shooting):
 
-################## shooting #####################
-################## transient (5) #####################
+    shooting_voltage = np.zeros(2*h+1)
+    for j in range(2*h+1):
+        shooting_voltage[j] = V_shooting[j]
+    #print (shooting_voltage)
 
-  for i in range(2*h+1):
+  ################## shooting #####################
+  ################## transient (5) #####################
 
-    t_sim = np.arange(i*T1, (i+1)*T1+deltat, deltat)
+    for i in range(2*h+1):
 
-    if (t_sim[-1] != T1):
-      t_sim = np.arange(i*T1, (i+1)*T1, deltat)
+      t_sim = np.arange(i*T1, (i+1)*T1+deltat, deltat)
 
-    #analysis in t0 = 0, T1, ..., (2N+1)T1
+      if (t_sim[-1] != T1):
+        t_sim = np.arange(i*T1, (i+1)*T1, deltat)
 
-    vc0 = shooting_voltage[i]
+      #analysis in t0 = 0, T1, ..., (2N+1)T1
 
-    vs = Vm1*np.sin(2*pi*f1*t_sim[0])
+      vc0 = shooting_voltage[i]
 
-    A1 = np.array([[R1, -R1],
-                  [-R1, (R1 + R2 + R3)]], np.float64)
+      vs = Vm1*np.sin(2*pi*f1*t_sim[0])
 
-    b = np.array([[vs], [-vc0]], np.float64)
-    
-    lin_SM_t0 = linalg.solve(A1,b)
+      A1 = np.array([[R1, -R1],
+                    [-R1, (R1 + R2 + R3)]], np.float64)
 
-    i0 = float (lin_SM_t0[1]) #current i(0), i(T1),..., (2N)T1
-
-    for t in np.delete(t_sim, 0):
+      b = np.array([[vs], [-vc0]], np.float64)
       
-        vs = Vm1*np.sin(2*pi*f1*t) #voltage source
+      lin_SM_t0 = linalg.solve(A1,b)
+
+      i0 = float (lin_SM_t0[1]) #current i(0), i(T1),..., (2N)T1
+
+      for t in np.delete(t_sim, 0):
         
-        #linear system to solve in each t  
-        A2 = np.array([[R1, -R1, 0],
-            [-R1, (R1 + R2 + R3), 1],
-            [0, -deltat/(2*C), 1]], np.float64)
-        
-        b = np.array([[vs], [0], [vc0 + i0*deltat/(2*C)]], np.float64)
+          vs = Vm1*np.sin(2*pi*f1*t) #voltage source
+          
+          #linear system to solve in each t  
+          A2 = np.array([[R1, -R1, 0],
+              [-R1, (R1 + R2 + R3), 1],
+              [0, -deltat/(2*C), 1]], np.float64)
+          
+          b = np.array([[vs], [0], [vc0 + i0*deltat/(2*C)]], np.float64)
 
-        z = linalg.solve(A2, b) #solution of system in z
+          z = linalg.solve(A2, b) #solution of system in z
 
-        #capacitor current 
-        i0 = float (z[1])
+          #capacitor current 
+          i0 = float (z[1])
 
-        #capacitor voltage
-        vc0 = float (z[2])
+          #capacitor voltage
+          vc0 = float (z[2])
 
-    transient_result[i] = vc0
+      transient_result[i] = vc0
 
-  return np.concatenate([
-    transient_result - D@shooting_voltage
-    ])
+    return np.concatenate([
+      transient_result - D@shooting_voltage
+      ])
 
-#solve QPSS function
-amplitudes_guess = np.zeros(5)
-y = fsolve(QPSS, amplitudes_guess)
-print (y)
-t_sim = np.arange(0, 5*1/f2 + 1/(100 * f2), 1/(100 * f2))
+  #solve QPSS function
+  amplitudes_guess = np.zeros(2*h+1)
+  y = fsolve(QPSS, amplitudes_guess)
+  #print (y)
 
-for t in t_sim:
-    Vc_time =  y[0] +  y[1]*sin(w2*t)  + y[2]*cos(w2*t)  + y[3]*sin(2*w2*t)  + y[4]*cos(2*w2*t) 
-    
-    results_vc.append (Vc_time)
+  t_sim = np.arange(0, 5*1/f2 + 1/(100 * f2), 1/(100 * f2))
+
+  for t in t_sim:
+
+      sinandcos = np.array([1] + [f(w2*(j+1)*t) for j in range(h) for f in (sin, cos)])
+      Vc_time = dot(y, sinandcos)
+      #print(Vc_time)
+
+      results_vc.append (Vc_time)
+  
+
+  plt.plot (t_sim, results_vc, label = f'H = {h}')
+  plt.title ('Tensão no capacitor')
+  plt.ylabel ('(V)')
+  plt.xlabel ('Tempo (milisegundos)')
+  plt.grid()
+  plt.legend()
+  
 
 #plot results
-plt.plot (t_sim, results_vc)
-plt.title ('Tensão no capacitor')
-plt.ylabel ('(V)')
-plt.xlabel ('Tempo (milisegundos)')
-plt.grid()
-plt.show ()
+plt.show()
---
+++ testes.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import sin, cos, pi
+from numpy import linalg, pi
+from scipy.optimize.minpack import fsolve
+
+#ICs
+R1 = 1
+R2 = 2
+R3 = 3
+C = 1
+Vm1 = 60
+Vm2 = 40
+f1 = 100
+w1 = 2*pi*f1
+f2 = 8
+w2 = 2*pi*f2
+deltat = 1/(100 * f1)
+T1 = (1/f1)
+T = (1/f2)
+h = 2
+
+#frequency -> time
+gamma_inv = np.array([[1, sin(2*pi*0*1/(2*h+1)), cos(2*pi*0*1/(2*h+1)), sin(2*pi*0*2/(2*h+1)), cos((2*pi*0*2)/(2*h+1))],
+                      [1, sin(2*pi*1*1/(2*h+1)), cos(2*pi*1*1/(2*h+1)), sin(2*pi*1*2/(2*h+1)), cos((2*pi*1*2)/(2*h+1))],
+                      [1, sin(2*pi*2*1/(2*h+1)), cos(2*pi*2*1/(2*h+1)), sin(2*pi*2*2/(2*h+1)), cos((2*pi*2*2)/(2*h+1))],
+                      [1, sin(2*pi*3*1/(2*h+1)), cos(2*pi*3*1/(2*h+1)), sin(2*pi*3*2/(2*h+1)), cos((2*pi*3*2)/(2*h+1))],
+                      [1, sin(2*pi*4*1/(2*h+1)), cos(2*pi*4*1/(2*h+1)), sin(2*pi*4*2/(2*h+1)), cos((2*pi*4*2)/(2*h+1))]])
+
+#frequency -> time, one period ahead
+gamma_inv_T1 = np.array([[1, sin(1*(2*pi*f2*(0*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(0*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(0*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(0*T/(2*h+1)+T1)))],
+                         [1, sin(1*(2*pi*f2*(1*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(1*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(1*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(1*T/(2*h+1)+T1)))],
+                         [1, sin(1*(2*pi*f2*(2*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(2*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(2*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(2*T/(2*h+1)+T1)))],
+                         [1, sin(1*(2*pi*f2*(3*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(3*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(3*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(3*T/(2*h+1)+T1)))],
+                         [1, sin(1*(2*pi*f2*(4*T/(2*h+1)+T1))), cos(1*(2*pi*f2*(4*T/(2*h+1)+T1))), sin(2*(2*pi*f2*(4*T/(2*h+1)+T1))), cos(2*(2*pi*f2*(4*T/(2*h+1)+T1)))]])
+
+#time -> frequency
+gamma = linalg.inv(gamma_inv)
+
+#delay matrix
+D = gamma_inv_T1@gamma
+
+#vectors to storage results
+results_vc = []
+
+transient_result = np.zeros(5)
+
+def QPSS(V_shooting):
+
+  shooting_voltage = np.zeros(5)
+  for j in range(5):
+      shooting_voltage[j] = V_shooting[j]
+  print (shooting_voltage)
+
+################## shooting #####################
+################## transient (5) #####################
+
+  for i in range(2*h+1):
+
+    t_sim = np.arange(i*T1, (i+1)*T1+deltat, deltat)
+
+    if (t_sim[-1] != T1):
+      t_sim = np.arange(i*T1, (i+1)*T1, deltat)
+
+    #analysis in t0 = 0, T1, ..., (2N+1)T1
+
+    vc0 = shooting_voltage[i]
+
+    vs = Vm1*np.sin(2*pi*f1*t_sim[0])
+
+    A1 = np.array([[R1, -R1],
+                  [-R1, (R1 + R2 + R3)]], np.float64)
+
+    b = np.array([[vs], [-vc0]], np.float64)
+    
+    lin_SM_t0 = linalg.solve(A1,b)
+
+    i0 = float (lin_SM_t0[1]) #current i(0), i(T1),..., (2N)T1
+
+    for t in np.delete(t_sim, 0):
+      
+        vs = Vm1*np.sin(2*pi*f1*t) #voltage source
+        
+        #linear system to solve in each t  
+        A2 = np.array([[R1, -R1, 0],
+            [-R1, (R1 + R2 + R3), 1],
+            [0, -deltat/(2*C), 1]], np.float64)
+        
+        b = np.array([[vs], [0], [vc0 + i0*deltat/(2*C)]], np.float64)
+
+        z = linalg.solve(A2, b) #solution of system in z
+
+        #capacitor current 
+        i0 = float (z[1])
+
+        #capacitor voltage
+        vc0 = float (z[2])
+
+    transient_result[i] = vc0
+
+  return np.concatenate([
+    transient_result - D@shooting_voltage
+    ])
+
+#solve QPSS function
+amplitudes_guess = np.zeros(5)
+y = fsolve(QPSS, amplitudes_guess)
+print (y)
+t_sim = np.arange(0, 5*1/f2 + 1/(100 * f2), 1/(100 * f2))
+
+for t in t_sim:
+    Vc_time =  y[0] +  y[1]*sin(w2*t)  + y[2]*cos(w2*t)  + y[3]*sin(2*w2*t)  + y[4]*cos(2*w2*t) 
+    
+    results_vc.append (Vc_time)
+
+#plot results
+plt.plot (t_sim, results_vc)
+plt.title ('Tensão no capacitor')
+plt.ylabel ('(V)')
+plt.xlabel ('Tempo (milisegundos)')
+plt.grid()
+plt.show ()
