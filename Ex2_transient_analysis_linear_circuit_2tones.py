61a6b783ff4c8b34d0bf543a1a29d37018f8032a
Jose Eduardo <joseduardosilva.42@gmail.com>
2021-12-14 18:50:01 -0300

    14-12-MNA add

--- Ex2_transient_analysis_linear_circuit_2tones.py
+++ Ex2_transient_analysis_linear_circuit_2tones.py
@@ -20,7 +20,7 @@ Vm2 = 40
 f1 = 100
 f2 = 10
 deltat = 1/(100 * f1)
-tf = 3*(1/f2)
+tf = (1/f2)
 vc0 = 0.17842857
 
 t_sim = np.arange(0, tf+deltat, deltat) #time vector to simulation, without t0
@@ -32,16 +32,15 @@ vs = Vm1*np.sin(2*pi*f1*t_sim[0]) + Vm2*np.sin(2*pi*f2*t_sim[0])#voltage in sour
 #The matrix from MNA
 A1 = np.array([[1, 0, 0, 0, 0],
                [(1/R1)+(1/R2), -1/R2, 0, 1, 0],
-               [-1/R2, 1/R2, 0, 0, -1],
+               [-1/R2, 1/R2, 0, 0, 1],
                [0, 0, -1/R3, 0, 1],
                [0, 1, -1, 0, 0]])
           
 b = np.array([[vs], [0], [0], [0], [vc0]], np.float64)
 
-x = linalg.solve(A1, b)#solving the system of linear equations in t x = [Va, Vb, Vc, is, ic1]
+x = linalg.solve(A1, b)#solving the system of linear equations in t. x = [Va, Vb, Vc, is, ic1]
 
 i0 = float (x[4]) #current in t0, to use in interations in
-print (i0)
 
 result_vc = [] #vector to storage the voltage at capacitor
 result_ic = [] #vector to storage the current at capacitor
@@ -51,7 +50,6 @@ result_is = [] #vector to storage the current at source
 result_vc.append (vc0)
 result_ic.append (i0)
 result_is.append (float (x[3]))
-print (float(x[3]))
 
 for t in np.delete(t_sim, 0):
 
@@ -60,13 +58,13 @@ for t in np.delete(t_sim, 0):
     #The matrix from MNA         
     A2 = np.array([[1, 0, 0, 0, 0],
                    [(1/R1)+(1/R2), -1/R2, 0, 1, 0],
-                   [-1/R2, 1/R2, 0, 0, -1],
+                   [-1/R2, 1/R2, 0, 0, 1],
                    [0, 0, -1/R3, 0, 1],
                    [0, 1, -1, 0, -deltat/(2*C)]], np.float64)
 
     b = np.array([[vs], [0], [0], [0], [vc0 + i0*deltat/(2*C)]], np.float64)
 
-    x = linalg.solve(A2, b)  #solving the system of linear equations in t x = [Va, Vb, Vc, is, ic1]
+    x = linalg.solve(A2, b)  #solving the system of linear equations in t. x = [Va, Vb, Vc, is, ic1]
     
     #current at source
     i_s = float (x[3])
@@ -81,7 +79,7 @@ for t in np.delete(t_sim, 0):
     vc = float (x[1] - x[2])
     result_vc.append (vc)
     vc0 = vc
-    #print (i_s)
+    print (i_s)
 
 #plot transient analysis
 plt.plot (t_sim, result_vc)
