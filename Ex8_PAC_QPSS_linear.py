d2e5e8633b87e5bb4549adab84990662d239ce59
joseduardo <joseduardosilva.42@gmail.com>
2022-05-16 16:54:11 -0300

    Adjustment of QPSS to nonlinear circuit

---
+++ Ex8_PAC_QPSS_linear.py
@@ -0,0 +1,105 @@
+from math import sin, cos, pi
+import numpy as np
+from numpy.linalg import inv
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+
+from Ex7_PAC_HB_nonlinear import h1, k, x1_1tom
+from Ex3_transien_nonlinear_circuit import nonlinear_element as nonlinear_element_transient
+
+"""
+This code have as objective the harmonic balance analysis of a non linear
+circuit. To calculate the harmonic balance, it is necessary to provide the initial
+conditions in the components of circuits and also the equations of circuit.
+The simulation of transient depends of the wave amplitudes obtained in the
+harmonic balance
+"""
+
+# params
+C1 = 10 * 10 ** -12
+C2 = 1 * 10 ** -6
+R1 = 1 * 10 ** 3
+RL = 50
+f1 = 1 * 10 ** 9
+w1 = 2 * pi * f1
+f2 = 1.1 * 10 ** 9
+w2 = 2 * pi * f2
+Vm1 = 5
+Vm2 = 0.2
+h2 = int(h1 / 2)
+k2 = 2 * (2 * h2 + 1)
+T1 = (1 / f1)
+T = (1 / f2)
+
+# frequency -> time (F = gamma_inv)
+gamma_inv = np.array([[1] + [f(2 * pi * i * (j + 1) / k) for j in range(h1) for f in (sin, cos)] for i in
+                      range(k)])
+
+# time -> frequency (F⁻¹ = gamma)
+gamma = inv(gamma_inv)
+
+# omega_2tons matrix
+omega_2tons = np.zeros((k2, k2))
+position_omega_vector = 0
+for h in np.arange(-h2, h2 + 1, 1):
+    omega_2tons[2 * position_omega_vector + 1, 2 * position_omega_vector] = - (w2 + h * w1)
+    omega_2tons[2 * position_omega_vector, 2 * position_omega_vector + 1] = (w2 + h * w1)
+    position_omega_vector += 1
+
+# creating G_1tom (first matrix in right hand of (22))
+non_linear = gamma_inv @ x1_1tom
+
+g_1tom = gamma @ ((0.1 * np.sign(non_linear)) / ((1 + (1.8 / abs(non_linear)) ** 5) ** (1 / 5)))
+for p in np.delete(range(len(g_1tom)), 0):
+    g_1tom[p] = g_1tom[p]/2
+
+G_1tom = np.zeros((k2, k2))
+for i in range(k2):
+    if i % 2 == 0:
+        r1 = range(i, -1, -1)
+        r2 = range(i + 2, k2)
+    else:
+        r1 = range(i, k2)
+        r2 = range(i-2, -1, -1)
+
+    for (n, j) in enumerate(r1):
+        G_1tom[i, j] = g_1tom[n]
+
+    for (n, j) in enumerate(r2):
+        if n % 2 == 0:
+            G_1tom[i, j] = g_1tom[n+2]
+        else:
+            G_1tom[i, j] = -g_1tom[n]
+
+
+def hb_lin(v):
+    # vector of unknowns
+    Va = np.zeros(k2)
+    Vb = np.zeros(k2)
+    Vc = np.zeros(k2)
+    for j in range(k2):
+        Va[j] = v[j]
+        Vb[j] = v[k2 + j]
+        Vc[j] = v[2 * k2 + j]
+
+    # definition of amplitude source and Va in time-domain
+    A_amplitude = np.zeros(k2)
+    A_amplitude[h1 + 1] = Vm2
+    vc2 = Vb - Vc
+
+    return np.concatenate([
+        Va - A_amplitude,
+        G_1tom @ Va - (1 / R1) * Vb
+        - (C1 * omega_2tons) @ Vb - (C2 * omega_2tons) @ vc2,
+        C2 * omega_2tons @ vc2 - Vc / RL
+    ])
+
+
+amplitudes_guess = np.zeros(3 * k2)
+linear_result = fsolve(hb_lin, amplitudes_guess)
+print(linear_result)
+x1_lin = linear_result[:k2]
+x2_lin = linear_result[k2: 2 * k2]
+x3_lin = linear_result[2 * k2: 3 * k2]
+X_c1_lin = (C1 * omega_2tons) @ x2_lin
---
+++ Ex8_PAC_QPSS_nonlinear.py
@@ -0,0 +1,192 @@
+import numpy as np
+from numpy import linalg, pi
+import matplotlib.pyplot as plt
+from math import sin, cos, pi
+from scipy.optimize.minpack import fsolve
+from Ex2_transient_analysis_linear_circuit_2tones import t_sim_trans, result_vc_trans
+
+# ICs
+C1 = 10 * 10 ** -12
+C2 = 1 * 10 ** -6
+R1 = 1 * 10 ** 3
+RL = 50
+f1 = 1 * 10 ** 9
+w1 = 2 * pi * f1
+f2 = 1.1 * 10 ** 9
+w2 = 2 * pi * f2
+Vm1 = 5
+Vm2 = 0.2
+h1 = 2
+k = 2 * h1 + 1
+final_resnorm = 0
+
+T1 = (1 / f1)
+T = (1 / f2)
+
+for h in range(2, 3):
+    k = 2 * h + 1
+    # frequency -> time
+    gamma_inv = np.array([[1] + [f(2 * pi * i * (j + 1) / k) for j in range(h) for f in (sin, cos)] for i in
+                          range(k)])
+
+    # frequency -> time, one period ahead
+    gamma_inv_T1 = np.array(
+        [[1] + [f(2 * pi * f2 * (j + 1) * (i * T / k + T1)) for j in range(h) for f in (sin, cos)] for i in
+         range(k)])
+
+    # time -> frequency
+    gamma = linalg.inv(gamma_inv)
+
+    # delay matrix
+    D = gamma_inv_T1 @ gamma
+
+    # vectors to storage results
+    shooting_Va = np.zeros(k)
+    shooting_Vb = np.zeros(k)
+    shooting_Vc = np.zeros(k)
+    shooting_is = np.zeros(k)
+    shooting_ic1 = np.zeros(k)
+    shooting_ic2 = np.zeros(k)
+
+    transient_Va = np.zeros(k)
+    transient_Vb = np.zeros(k)
+    transient_Vc = np.zeros(k)
+    transient_is = np.zeros(k)
+    transient_ic1 = np.zeros(k)
+    transient_ic2 = np.zeros(k)
+
+
+    def qpss(shooting_tension):
+
+        # vector of unknowns
+
+        for i in range(k):
+            shooting_Va[i] = shooting_tension[i]
+            shooting_Vb[i] = shooting_tension[k + i]
+            shooting_Vc[i] = shooting_tension[2 * k + i]
+            shooting_is[i] = shooting_tension[3 * k + i]
+            shooting_ic1[i] = shooting_tension[4 * k + i]
+            shooting_ic2[i] = shooting_tension[4 * k + i]
+
+        ################## shooting #####################
+        ################## transient (5) #####################
+
+        for i in range(k):
+
+            n = int(10 * f1 / f2)
+            (t_sim, deltat) = np.linspace(i * (T / k), i * (T / k) + T1, n, retstep=True)
+
+            vs = Vm1 * np.sin(2 * pi * f1 * t_sim[0])
+
+            Vc10 = shooting_Vb[i]
+            Vc20 = shooting_Vb[i] - shooting_Vc[i]
+
+            # system of nodal analysis to solve in actual time
+            def func(x):
+
+                if shooting_Va[i] == 0:
+                    i_fnl = 0
+                else:
+                    i_fnl = ((0.1 * np.sign(x[0])) / ((1 + (1.8 / abs(x[0])) ** 5) ** (1 / 5)))
+
+                return [shooting_Va[i] - vs,
+                        i_fnl - shooting_Vb[i] / R1
+                        - Vc10 - (x[1] - x[2]),
+                        Vc20 - x[2] / RL]
+
+            initial_conditions = fsolve(func, np.array(
+                [shooting_Va[i], shooting_Vb[i], shooting_Vc[i]]))  # solving the system of nonlinear equations in t
+
+            Va = float(initial_conditions[0])
+            Vb = float(initial_conditions[1])
+            Vc = float(initial_conditions[2])
+
+            if Va == 0:
+                ifnl = 0
+            else:
+                ifnl = ((0.1 * np.sign(Va)) / ((1 + (1.8 / abs(Va)) ** 5) ** (1 / 5)))
+
+            ic10 = ifnl - Vb / R1 - Vc / RL
+
+            ic20 = Vc / RL
+
+            for t in np.delete(t_sim, 0):
+                vs = Vm1 * np.sin(2 * pi * f1 * t)
+
+                # system of nodal analysis to solve in actual time
+                def func(x):
+
+                    if x[0] == 0:
+                        i_fnl = 0
+                    else:
+                        i_fnl = ((0.1 * np.sign(x[0])) / ((1 + (1.8 / abs(x[0])) ** 5) ** (1 / 5)))
+
+                    return [x[0] - vs,
+                            i_fnl - x[1] / R1 - (
+                                    (2 * C1 / deltat) * (x[1] - Vc10) - ic10) - (
+                                    (2 * C2 / deltat) * ((x[1] - x[2]) - Vc20) - ic20),
+                            -((2 * C2 / deltat) * ((x[1] - x[2]) - Vc20) - ic20) + x[2] / RL]
+
+                z = fsolve(func, np.array(
+                    [Va, Vb, Vc]))  # solving the system of nonlinear equations in t
+
+                # node voltages
+                Va = float(z[0])
+                Vb = float(z[1])
+                Vc = float(z[2])
+
+                # capacitor current
+                ic10 = ((2 * C1 / deltat) * (Va - Vc10) - ic10)
+                ic20 = ((2 * C1 / deltat) * ((Vb - Vc) - Vc10) - ic10)
+
+                # capacitor voltage
+                Vc10 = Vb
+                Vc20 = Vb - Vc
+
+            # transient results
+            transient_Va[i] = Va
+            transient_Vb[i] = Vb
+            transient_Vc[i] = Vc
+            transient_ic1[i] = ic10
+            transient_ic2[i] = ic20
+
+        return np.concatenate([
+            transient_Va - D @ shooting_Va,
+            transient_Vb - D @ shooting_Vb,
+            transient_Vc - D @ shooting_Vc,
+            transient_ic1 - D @ shooting_ic1,
+            transient_ic1 - D @ shooting_ic2
+        ])
+
+
+    # solve qpss function
+    amplitudes_guess = np.zeros(k * 5)
+    y = fsolve(qpss, amplitudes_guess, full_output=True)
+
+    # resnorm external fsolve
+    resnorm = sum(y[1]['fvec'] ** 2)
+    if resnorm > final_resnorm:
+        final_resnorm = resnorm
+    # print(final_resnorm)
+    # print (final_resnorm)
+
+    Y = gamma @ (y[0][k:2 * k] - y[0][2 * k:3 * k])
+
+    t_sim_waveform = np.array([i * T1 for i in range(int(f1 / f2) + 1)])
+
+    results_vc = []
+
+    for t_waveform in t_sim_waveform:
+        sinandcos = np.array([1] + [f(w2 * (j + 1) * t_waveform) for j in range(h) for f in (sin, cos)])
+        Vc_time = Y @ sinandcos
+        results_vc.append(Vc_time)
+
+    # plot results
+    plt.plot(t_sim_waveform, results_vc, "ob", label=f'H = {h}')
+    plt.plot(t_sim_trans, result_vc_trans)
+    plt.title('Tensão no capacitor')
+    plt.ylabel('(V)')
+    plt.xlabel('Tempo (segundos)')
+    plt.grid()
+    plt.legend()
+plt.show()
